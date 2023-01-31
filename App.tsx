af9f4915ba7bfc03b50130765d50209a13ba9d5c
Enzo Neault <aza.woodyy@gmail.com>
2023-01-31 15:00:54 +0100

    ➕  Added react-navigation + Changed on eslint rule

--- App.tsx
+++ App.tsx
@@ -1,34 +1,28 @@
-// eslint-disable-next-line import/no-extraneous-dependencies
 import { styled } from 'nativewind';
 import React, { ReactElement } from 'react';
 import {
-  SafeAreaView,
-  ScrollView,
-  Text,
-  View,
+	SafeAreaView,
+	ScrollView,
+	Text,
+	View,
 } from 'react-native';
-
-import {
-  Header,
-} from 'react-native/Libraries/NewAppScreen';
+import { Header } from 'react-native/Libraries/NewAppScreen';
 
 const StyledText = styled(Text);
 
 function App(): ReactElement {
-  return (
-    <SafeAreaView>
-      <ScrollView
-        contentInsetAdjustmentBehavior="automatic"
-      >
-        <Header />
-        <View>
-          <StyledText className="text-red-500">
-            YOP !
-          </StyledText>
-        </View>
-      </ScrollView>
-    </SafeAreaView>
-  );
+	return (
+		<SafeAreaView>
+			<ScrollView>
+				<Header />
+				<View>
+					<StyledText className="text-red-500">
+						YOP !
+					</StyledText>
+				</View>
+			</ScrollView>
+		</SafeAreaView>
+	);
 }
 
 export default App;
