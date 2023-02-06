de3cda4f2fc7cce0672b8e0e471026727f44f26f
Enzo Neault <aza.woodyy@gmail.com>
2023-02-06 15:17:37 +0100

    💄 Designed the bottom bar navigator

--- app/components/BottomNavigator.tsx
+++ app/components/BottomNavigator.tsx
@@ -1,17 +1,43 @@
+/* eslint-disable react/jsx-props-no-spreading */
+/* eslint-disable react/no-unstable-nested-components */
+/* eslint-disable no-nested-ternary */
+/* eslint-disable no-mixed-spaces-and-tabs */
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import React, { ReactElement } from 'react';
 import Home from '../screens/Home';
 import Profile from '../screens/Profile';
 import Messages from '../screens/Messages/Messages';
+import TabBar from './TabBar';
 
 const BottomTabNavigator = createBottomTabNavigator();
 
 function BottomNavigator(): ReactElement {
 	return (
-		<BottomTabNavigator.Navigator screenOptions={{ headerShown: false }}>
-			<BottomTabNavigator.Screen name="Home" component={Home} />
-			<BottomTabNavigator.Screen name="Settings" component={Profile} />
-			<BottomTabNavigator.Screen name="Messages" component={Messages} />
+		<BottomTabNavigator.Navigator
+			screenOptions={{ headerShown: false }}
+			tabBar={(props) => <TabBar {...props} />}
+		>
+			<BottomTabNavigator.Screen
+				name="Home"
+				component={Home}
+				options={{
+					//
+				}}
+			/>
+			<BottomTabNavigator.Screen
+				name="Settings"
+				component={Profile}
+				options={{
+					//
+				}}
+			/>
+			<BottomTabNavigator.Screen
+				name="Messages"
+				component={Messages}
+				options={{
+					//
+				}}
+			/>
 		</BottomTabNavigator.Navigator>
 	);
 }
--- app/screens/Home.tsx
+++ app/screens/Home.tsx
@@ -1,16 +1,19 @@
 import React, { ReactElement } from 'react';
 import { View } from 'react-native';
+import { styled } from 'nativewind';
 import HomeHeader from '../features/home/HomeHeader';
 import HomeContent from '../features/home/HomeContent';
 import HomeFooter from '../features/home/HomeFooter';
 
+const StyledView = styled(View);
+
 function Home(): ReactElement {
 	return (
-        <View>
+        <StyledView className="bg-neutral-50 flex-1">
             <HomeHeader title="Home Header" />
             <HomeContent title="Home Content" />
             <HomeFooter title="Home Footer" />
-        </View>
+        </StyledView>
 	);
 }
 
